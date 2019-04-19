a93109e5b59e3a276ce68ea8266f9c5feebb161c
anagha <anaghav1998@gmail.com>
2019-04-19 23:16:08 +0530

    added test cases 1

---
+++ app/src/test/java/myapp/bmtc/com/bmtc_2/bookTicketTest.java
@@ -0,0 +1,32 @@
+package myapp.bmtc.com.bmtc_2;
+
+import android.view.View;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class bookTicketTest {
+
+//    private static final Object View = ;
+
+    @Test
+    public void onCreate() {
+        assertEquals(4,2+2);
+    }
+
+    @Test
+    public void onClick() {
+       // View view;
+       // bookTicket bt = new bookTicket();
+
+//        if(view.getId() == R.id.quick)
+//        {
+//
+//        }
+
+        //assertEquals(view.getId(),R.id.quick);
+
+        assertEquals(6,4+2);
+    }
+}
